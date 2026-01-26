71d2ce3d8d84570074fc802597d2a556da5acfc8
Guillaume Lagrange <lagrange.guillaume.1@gmail.com>
2026-01-26 15:42:31 -0500

    Init repo

---
+++ xtask/src/main.rs
@@ -0,0 +1,59 @@
+#[macro_use]
+extern crate log;
+
+use std::time::Instant;
+use tracel_xtask::prelude::*;
+
+// no-std
+const WASM32_TARGET: &str = "wasm32-unknown-unknown";
+const ARM_TARGET: &str = "thumbv7m-none-eabi";
+const ARM_NO_ATOMIC_PTR_TARGET: &str = "thumbv6m-none-eabi";
+
+#[macros::base_commands(
+    Bump,
+    Check,
+    Compile,
+    Coverage,
+    Doc,
+    Dependencies,
+    Fix,
+    Publish,
+    Validate,
+    Vulnerabilities
+)]
+pub enum Command {
+    /// Build Burn ONNX in different modes.
+    Build(BuildCmdArgs),
+    /// Test Burn ONNX.
+    Test(TestCmdArgs),
+}
+
+fn main() -> anyhow::Result<()> {
+    let start = Instant::now();
+    let (args, environment) = init_xtask::<Command>(parse_args::<Command>()?)?;
+
+    if args.context == Context::NoStd {
+        // Install additional targets for no-std execution environments
+        rustup_add_target(WASM32_TARGET)?;
+        rustup_add_target(ARM_TARGET)?;
+        rustup_add_target(ARM_NO_ATOMIC_PTR_TARGET)?;
+    }
+
+    match args.command {
+        Command::Build(cmd_args) => {
+            base_commands::build::handle_command(cmd_args, environment, args.context)
+        }
+        Command::Test(cmd_args) => {
+            base_commands::test::handle_command(cmd_args, environment, args.context)
+        }
+        _ => dispatch_base_commands(args, environment),
+    }?;
+
+    let duration = start.elapsed();
+    info!(
+        "\x1B[32;1mTime elapsed for the current execution: {}\x1B[0m",
+        format_duration(&duration)
+    );
+
+    Ok(())
+}
