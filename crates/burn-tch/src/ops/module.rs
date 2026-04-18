0bdfbd9448c0d1167161f200d47d26a8baf97a38
Dilshod Tadjibaev <939125+antimora@users.noreply.github.com>
2026-04-18 13:06:04 -0500

    Revert tch rfft/irfft to todo!() stubs
    These will be implemented in a separate PR with proper input padding
    via the n parameter.

--- crates/burn-tch/src/ops/module.rs
+++ crates/burn-tch/src/ops/module.rs
@@ -498,22 +498,15 @@ impl<E: TchElement> ModuleOps<Self> for LibTorch<E> {
         TchTensor::new(tensor)
     }
 
-    fn rfft(signal: FloatTensor<Self>, dim: usize) -> (FloatTensor<Self>, FloatTensor<Self>) {
-        let complex = signal.tensor.fft_rfft(None::<i64>, dim as i64, "backward");
-        let re = TchTensor::new(complex.real().contiguous());
-        let im = TchTensor::new(complex.imag().contiguous());
-        (re, im)
+    fn rfft(_signal: FloatTensor<Self>, _dim: usize) -> (FloatTensor<Self>, FloatTensor<Self>) {
+        todo!("rfft is not supported for now in LibTorch")
     }
 
     fn irfft(
-        spectrum_re: FloatTensor<Self>,
-        spectrum_im: FloatTensor<Self>,
-        dim: usize,
+        _spectrum_re: FloatTensor<Self>,
+        _spectrum_im: FloatTensor<Self>,
+        _dim: usize,
     ) -> FloatTensor<Self> {
-        let freq_size = spectrum_re.shape()[dim];
-        let n = (freq_size - 1) * 2;
-
-        let complex = tch::Tensor::complex(&spectrum_re.tensor, &spectrum_im.tensor);
-        TchTensor::new(complex.fft_irfft(Some(n as i64), dim as i64, "backward"))
+        todo!("irfft is not supported for now in LibTorch")
     }
 }
